93511cd5acf0200337372406161f237c27a8dcb2
Sebastiaan-hva <180168321+Sebastiaan-hva@users.noreply.github.com>
2026-06-22 15:15:55 +0200

    Fix: set includes dir to root _includes (was not found under src/)

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,7 @@ module.exports = function (eleventyConfig) {
         dir: {
             input: "src",
             output: "public",
+            includes: "../_includes",
         },
     };
 };
