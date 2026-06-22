6f8823cfccc6de7ace1dab9ba9010c5fad2192d9
Sebastiaan-hva <180168321+Sebastiaan-hva@users.noreply.github.com>
2026-06-22 15:14:01 +0200

    Fix: replace dead aws.random.cat API with thecatapi.com, add fallback

--- src/_data/catpic.js
+++ src/_data/catpic.js
@@ -1,7 +1,10 @@
 const axios = require("axios");
 
 module.exports = async () => {
-    const result = await axios.get("https://aws.random.cat/meow");
-
-    return result.data.file;
+    try {
+        const result = await axios.get("https://api.thecatapi.com/v1/images/search");
+        return result.data[0].url;
+    } catch {
+        return "https://upload.wikimedia.org/wikipedia/commons/1/18/Dog_Breeds.jpg";
+    }
 };
